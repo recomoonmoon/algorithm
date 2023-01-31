183c4016f0ac1ef8acd9f1eba356e2db9840d728
recomoonmoon <1125624537@11.com>
2023-01-31 19:21:42 +0800

    增加了几道题

--- dp.py
+++ dp.py
@@ -75,13 +75,44 @@ def lengthOfLIS(nums) -> int:
      else:
          for i in range(1, len(nums)):
              for j in range(i):
-                 print(f"i= {i} j = {j}")
                  if nums[i] > nums[j]:
                      dp[i] = max(dp[j] + 1, dp[i])
-         print(dp)
          return max(dp)
 
 
 nums = [1,3,6,7,9,4,10,5,6]
 print(nums)
 print(lengthOfLIS(nums))
+
+#Question 4
+# 在一个火车旅行很受欢迎的国度，你提前一年计划了一些火车旅行。在接下来的一年里，你要旅行的日子将以一个名为 days 的数组给出。每一项是一个从 1 到 365 的整数。
+# 火车票有 三种不同的销售方式 ：
+# 一张 为期一天 的通行证售价为 costs[0] 美元；
+# 一张 为期七天 的通行证售价为 costs[1] 美元；
+# 一张 为期三十天 的通行证售价为 costs[2] 美元。
+# 通行证允许数天无限制的旅行。 例如，如果我们在第 2 天获得一张 为期 7 天 的通行证，那么我们可以连着旅行 7 天：第 2 天、第 3 天、第 4 天、第 5 天、第 6 天、第 7 天和第 8 天。
+print("Q4")
+class Solution:
+    def mincostTickets(self, days, costs) -> int:
+        dp = [0 for i in range(366)]
+
+        for i in range(len(days)):
+
+            if days[i] < 7:
+                dp[days[i]] = min(dp[days[i]-1] + costs[0], costs[1], costs[2])
+
+            elif days[i] < 30:
+                dp[days[i]] = min(dp[days[i]-1] + costs[0], dp[days[i]-7] + costs[1], costs[2])
+                #print(f"dp[days[i]] + costs[0] {dp[days[i]] + costs[0]}\ndp[days[i - 7]] + costs[1] {dp[days[i]] + costs[0]}")
+            else:
+                dp[days[i]] = min(dp[days[i]-1] + costs[0], dp[days[i]-7] + costs[1], dp[days[i] - 30] + costs[2])
+                #print(f"dp[days[i]] + costs[0] {dp[days[i]] + costs[0]}\ndp[days[i - 7]] + costs[1] {dp[days[i]] + costs[0]}\ndp[days[i] - 30] + costs[2] {dp[days[i] - 30] + costs[2]}")
+            dp[days[i]:] = [dp[days[i]] for k in range(len(dp[days[i]:]))]
+            #print(dp, "\n")
+        return dp[-1]
+
+
+days = [1,4,6,7,8,20]
+costs = [2,7,15]
+s = Solution()
+print(s.mincostTickets(days, costs))
